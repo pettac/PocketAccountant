6b3a7702b3cdcae074b915dcad3cf99da57610de
Chris <cpetta91@gmail.com>
2016-10-25 20:49:37 -0400

    Messing around with layouts and creating dummy activities that will be removed later on

---
+++ app/src/main/java/cst4701/game/pocketaccountant/Continue.java
@@ -0,0 +1,13 @@
+package cst4701.game.pocketaccountant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Continue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_continue);
+    }
+}
--- app/src/main/java/cst4701/game/pocketaccountant/MainActivity.java
+++ app/src/main/java/cst4701/game/pocketaccountant/MainActivity.java
@@ -1,7 +1,9 @@
 package cst4701.game.pocketaccountant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNewGame(View view){
+        Intent intent = new Intent(this, NewGame.class);
+        startActivity(intent);
+    }
+
+    public void openContinue(View view){
+        Intent intent = new Intent(this, Continue.class);
+        startActivity(intent);
+    }
+
+    public void openSettings(View view){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
